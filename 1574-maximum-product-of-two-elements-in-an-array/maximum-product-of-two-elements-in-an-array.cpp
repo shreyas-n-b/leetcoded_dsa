884cc1dd32aebc7cfd331912c1d4cb9a31222451
shreyas-n-b <shreyubiradar@gmail.com>
2026-07-27 12:55:46 +0530

    Time: 0 ms (100.00%) | Memory: 13.5 MB (25.65%) - LeetSync

--- 1574-maximum-product-of-two-elements-in-an-array/maximum-product-of-two-elements-in-an-array.cpp
+++ 1574-maximum-product-of-two-elements-in-an-array/maximum-product-of-two-elements-in-an-array.cpp
@@ -5,17 +5,15 @@ public:
         int maxi=0;
         int ind=-1;
         for(int i=0; i<n; i++){
-            if(nums[i]>1 && nums[i]>maxi){
+            if(nums[i]>maxi){
                 maxi=nums[i];
                 ind=i;
             }
         }
-        if(maxi==0)return 0;
         int a=maxi;
         maxi=0;
         for(int i=0; i<n; i++){
-            if(i==ind)continue;
-            if(nums[i]>1 && nums[i]>maxi){
+            if(i!=ind && nums[i]>maxi){
                 maxi=nums[i];
             }
         }
