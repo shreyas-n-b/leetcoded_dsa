08ec13fb477b18cd8aed8d121d78faefd100c2a0
shreyas-n-b <shreyubiradar@gmail.com>
2026-07-29 19:39:09 +0530

    Time: 33 ms (43.48%) | Memory: 17.1 MB (63.77%) - LeetSync

--- 3813-smallest-palindromic-rearrangement-ii/smallest-palindromic-rearrangement-ii.cpp
+++ 3813-smallest-palindromic-rearrangement-ii/smallest-palindromic-rearrangement-ii.cpp
@@ -1,123 +1,77 @@
-/*class Solution {
-private:
-    static const double limit=1e307
-    double f(double numerator, vector<double>& denominators){
-        //validation
-        double sum=0.0;
-        for(double d: denominators){
-            sum += d;
-        }
-        if(sum != numerator)return -1.0;
-        //implementation
-        int n=denominators.size();
-        int i=0;
-        double ans=1.0;
-        while(numerator > 0){
-            if(denominators[i]==0){
-                i++;
-            }
-            if(i >= n)break;
-            double up=numerator--;
-            double down=denominators[i]--;
-            if(ans > limit/(up/down))return limit;
-            ans = ans*(up/down);
-        }
-        return ans;
-    }
-public:
-    string smallestPalindrome(string s, int k) {
-        
-    }
-};*/
 class Solution {
-public:
-
-    long long nCr(int n, int r, int k) {
-        r = min(r, n-r); //nCr == nC(n-r)
-
-        long long result = 1;
-
-        for(int i = 1; i <= r; i++) {//O(log2(k))
-            result = result * (n - r + i)/i; //result is becoming twice
-
-            if(result >= k) 
-                return k;
+private: 
+    int nCr(int n, int r, int k){
+        r=min(r,n-r);
+        long long result=1;
+        for(int i=1; i<=r; i++){
+            result = result*(n-r+i)/i;
+            if(result>=k)return k;
         }
-
-        return result;
+        return (int)result;
     }
-
+    int countWays(vector<int>& count, int k){
+        int letters=0;
+        for(int i=0; i<26; i++){
+            letters += count[i];
+        }
+        long long ways=1;
+        for(int i=0; i<26; i++){
+            if(count[i]>0){
+                ways = ways*nCr(letters,count[i],k);
+                letters -= count[i];
+                if(ways >= k)return k;
+            }
+        }
+        return (int)ways;
+    }
+public:
     string smallestPalindrome(string s, int k) {
-        int n = s.length();
+        int n=s.size();
+        char mid=' ';
+        if(n%2==1){
+            mid=s[n/2];
+        }
+        string halfResult="";
+        int half=n/2;
+        vector<int> count(26,0);
 
-        char mid = ' ';
-        if(n%2 == 1) { //odd length
-            mid = s[n/2];
+        for(int i=0; i<n; i++){
+            if(n%2==1 && i==n/2)continue;
+            count[s[i]-'a']++;
         }
 
-        vector<int> count(26, 0);
-        for(int i = 0; i < n; i++) {
-            if(n%2 == 1 && i == n/2) continue; //mid character reserverd for middle one
-            count[s[i] - 'a']++;
-        }
+        for(int i=0; i<26; i++)count[i] /= 2;
 
-        //half frequency will be used to build halfResult
-        for(int i = 0; i < 26; i++) {
-            count[i] /= 2;
-        }
+        int assigned=0;
 
-        string halfResult = "";
-        int half = n/2;
-
-        for(int i = 0; i < half; i++) { //O(n/2)
-            //I am trying to fill ith position
-            //What if I could never fill a character in ith position
-            bool placedCharacter = false; //in ith position
-            for(int j = 0; j < 26; j++) { //which character to put
-                if(count[j] > 0) {
-                    count[j] -= 1;
-
-                    //count number of ways
-                    long long ways = 1;
-                    int letters = 0;
-                    for(int c = 0; c < 26; c++) {
-                        letters += count[c];
-                    }
-
-                    for(int c = 0; c < 26; c++) {
-                        if(count[c] > 0) {
-                            ways *= nCr(letters, count[c], k); //log2(k)
-                            letters -= count[c];
-                        }
-
-                        if(ways >= k) {
-                            break;
-                        }
-                    }
-
-                    if(ways >= k) { //this block contains my kth one
-                        halfResult.push_back(j + 'a'); //fixed this character at ith position
-                        placedCharacter = true;
+        for(int j=0; j<half; j++){
+            for(int i=0; i<26; i++){
+                if(count[i]>0){
+                    halfResult.push_back((char)('a'+i));
+                    count[i]=count[i]-1;
+                    int ways=countWays(count,k);
+                    if(ways >= k){
+                        assigned++;
                         break;
                     }
-
-                    k -= ways; //when k >= ways
-                    count[j] += 1;
+                    else{
+                        halfResult.pop_back();
+                        count[i]++;
+                        k=k-ways;                        
+                    }
                 }
             }
-
-            if(placedCharacter == false)
-                return "";
         }
 
-        //halfResult + mid + (reverse of halfResult)
-        string rev = halfResult;
-        reverse(begin(rev), end(rev)); //O(n/2)
-
-        if(mid != ' ') {
-            halfResult.push_back(mid);
+        string result="";
+        if(assigned < half)return result;
+        result += halfResult;
+        if(n%2==1){
+            result.push_back(mid);            
         }
-
-        return halfResult + rev;
+        for(int i=half-1; i>=0; i--){
+            result.push_back(halfResult[i]);
+        }
+        return result;
     }
 };
