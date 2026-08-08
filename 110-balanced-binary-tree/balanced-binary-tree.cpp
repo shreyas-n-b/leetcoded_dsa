6d1437d764d84b9d351a28f7288fecee67fe0e38
shreyas-n-b <shreyubiradar@gmail.com>
2026-08-08 15:19:14 +0530

    Time: 3 ms (9.52%) | Memory: 25.2 MB (21.06%) - LeetSync

--- 110-balanced-binary-tree/balanced-binary-tree.cpp
+++ 110-balanced-binary-tree/balanced-binary-tree.cpp
@@ -10,17 +10,28 @@
  * };
  */
 class Solution {
+private:
+    unordered_map<TreeNode*,int> mpp;
 public:
     int height(TreeNode* root){
         if(root == nullptr)return 0;
-        return (1+max(height(root->left), height(root->right)));
+        int leftheight=height(root->left);
+        int rightheight=height(root->right);
+        return mpp[root]=1+max(leftheight,rightheight);
     }
-    bool isBalanced(TreeNode* root) {
+    bool helper(TreeNode* root) {
         if(root == nullptr){
             return true;
         }
-        int balFactor=height(root->left)-height(root->right);
+        int leftfactor=(root->left==nullptr)?0:mpp[root->left];
+        int rightfactor=(root->right==nullptr)?0:mpp[root->right];
+        int balFactor=leftfactor-rightfactor;
         if(balFactor != 0 && balFactor != -1 && balFactor != 1)return false;
-        return (isBalanced(root->left) && isBalanced(root->right));       
+        return (helper(root->left) && helper(root->right));       
+    }
+    bool isBalanced(TreeNode* root){
+        if(root == nullptr)return true;
+        mpp[root]=max(height(root->left),height(root->right))+1;
+        return helper(root);
     }
 };
