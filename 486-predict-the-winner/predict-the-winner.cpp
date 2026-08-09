2a30a25faeb30a9cf4dd43f73d813173c005a47f
shreyas-n-b <shreyubiradar@gmail.com>
2026-08-09 12:55:25 +0530

    Time: 82 ms (19.28%) | Memory: 9.7 MB (68.53%) - LeetSync

--- 486-predict-the-winner/predict-the-winner.cpp
+++ 486-predict-the-winner/predict-the-winner.cpp
@@ -1,52 +1,18 @@
 class Solution {
 private:
-    int pOneScore=0;
-    int pTwoScore=0;
-    bool f(bool turn, int l, int r, vector<int>& nums, vector<vector<vector<int>>>& dp){
-        if(l == r){
-            if(turn){
-                pOneScore += nums[l];
-                bool temp=(pOneScore >= pTwoScore);
-                pOneScore -= nums[l];
-                return (bool)(dp[turn][l][l]=temp);
-            }else{
-                pTwoScore += nums[l];
-                bool temp=(pOneScore < pTwoScore);
-                pTwoScore -= nums[l];
-                return (bool)(dp[turn][l][l]=!temp);
-            }
-        }
-        if(dp[turn][l][r]!=-1)return (bool)dp[turn][l][r];
-        bool leftpick;
-        bool rightpick;
-        if(turn){
-            pOneScore += nums[l];
-            leftpick=f(!turn, l+1, r, nums, dp);
-            pOneScore -= nums[l];
-
-            pOneScore += nums[r];
-            rightpick=f(!turn, l, r-1, nums, dp);
-            pOneScore -= nums[r];
-        }else{
-            pTwoScore += nums[l];
-            leftpick=f(!turn, l+1, r, nums, dp);
-            pTwoScore -= nums[l];
-
-            pTwoScore += nums[r];
-            rightpick=f(!turn, l, r-1, nums, dp);
-            pTwoScore -= nums[r];
-        }
-
-        if(turn){
-            return (leftpick || rightpick);
-        }else{
-            return (leftpick && rightpick);
-        }
+    int solve(int i, int j, vector<int>& nums){
+        if(i>j)return 0;
+        if(i==j)return nums[i];
+        int takei=nums[i]+min(solve(i+2,j,nums),solve(i+1,j-1,nums));
+        int takej=nums[j]+min(solve(i+1,j-1,nums),solve(i,j-2,nums));
+        return max(takei,takej);
     }
 public:
     bool predictTheWinner(vector<int>& nums) {
         int n=nums.size();
-        vector<vector<vector<int>>> dp(2,vector<vector<int>>(n,vector<int>(n,-1)));
-        return f(true,0,nums.size()-1,nums,dp);        
+        int totalScore=accumulate(nums.begin(),nums.end(),0);
+        int player1score=solve(0,n-1,nums);
+        int player2score=totalScore-player1score;
+        return player1score >= player2score;        
     }
 };
